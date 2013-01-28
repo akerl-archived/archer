8dc24f94b91a542c37487179e43d4e6378540125
Les Aker <me@lesaker.org>
2013-01-27 21:01:35 -0500

    unicode in tty output does unhappy things

--- scripts/pacmankeygen.sh
+++ scripts/pacmankeygen.sh
@@ -13,14 +13,14 @@ if [ -n "$pkPID" ] ; then
     while [ -n "$(ps aux | grep [p]acman-key)" ] ; do mandb &> /dev/null ; done
   fi
 
-  gpg --homedir /etc/pacman.d/gnupg/ --no-permission-warning \
-    --import /usr/share/pacman/keyrings/archlinux.gpg
+  gpg --no-tty --homedir /etc/pacman.d/gnupg/ --no-permission-warning \
+    --import /usr/share/pacman/keyrings/archlinux.gpg &> /dev/null
 
   for keyid in $(cat /usr/share/pacman/keyrings/archlinux-trusted | sed 's/:.*//') ; do 
     pacman-key --lsign-key $keyid
   done
 
-  gpg --homedir /etc/pacman.d/gnupg/ --no-permission-warning \
-    --import-ownertrust /usr/share/pacman/keyrings/archlinux-trusted
+  gpg --no-tty --homedir /etc/pacman.d/gnupg/ --no-permission-warning \
+    --import-ownertrust /usr/share/pacman/keyrings/archlinux-trusted &> /dev/null
 fi
 

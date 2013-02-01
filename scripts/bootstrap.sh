c3aef9cf8b188ce33e6f7f2ec3d73fede28c65a2
Les Aker <me@lesaker.org>
2013-02-01 05:22:44 -0500

    needs -k since we use keys nao

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ansible all -m script -a scripts/bootstrap.sh
+# ansible all -k -m script -a scripts/bootstrap.sh
 
 mkdir -p /root/.ssh
 curl -s 'https://raw.github.com/akerl/keys/master/ender.pub' > /root/.ssh/authorized_keys
